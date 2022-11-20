1df608e221fb0ec7ec16ddfe9b2a4704bf7af5f7
harrymontenegro <harryjm018@hotmail.com>
2022-11-20 09:08:32 -0500

    clase 410

---
+++ webapp/src/main/java/org/aguzman/apiservlet/webapp/form/FormServlet.java
@@ -0,0 +1,56 @@
+package org.aguzman.apiservlet.webapp.form;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/registro")
+public class FormServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String email = req.getParameter("email");
+
+        try (PrintWriter out = resp.getWriter()) {
+            out.println("<!DOCTYPE>");
+            out.println("<html>");
+            out.println("  <head>");
+            out.println("    <meta charset=\"UTF-8\">");
+            out.println("    <title>Registro formulario</title>");
+            out.println("  </head>");
+            out.println("  <body>");
+            out.println("    <h1>Registro formulario!</h1>");
+            out.println("    <ul>");
+            out.println("       <li>Username: " + username + "</li>");
+            out.println("       <li>Password: " + password + "</li>");
+            out.println("       <li>Email: " + email + "</li>");
+            out.println("    </ul>");
+            out.println("  </body>");
+            out.println("</html>");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
